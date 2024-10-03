584e746740416e7212d5f425d4b05e23a830e15a
Marc-Eric Boury <meboury@hotmail.com>
2024-10-03 11:44:39 -0400

    Added Cours service + Cours view

--- 420DA3_A24_07453_Demo_Iterative/Business/Services/CoursService.cs
+++ 420DA3_A24_07453_Demo_Iterative/Business/Services/CoursService.cs
@@ -1,11 +1,31 @@
 ﻿using _420DA3_Demo_Iterative.DataAccess.DAOs;
+using _420DA3_Demo_Iterative.Presentation.Views;
+using System.Data;
 
 namespace _420DA3_Demo_Iterative.Business.Services;
 public class CoursService {
     private CoursDAO coursesDAO;
+    private CoursView coursView;
 
     public CoursService() {
         this.coursesDAO = new CoursDAO();
+        this.coursView = new CoursView(this);
+    }
+
+    public void OpenCoursWindow() {
+        _ = this.coursView.ShowDialog();
+    }
+
+    public DataTable GetCoursDataTable() {
+        return this.coursesDAO.GetDataTable();
+    }
+
+    public void SaveChanges() {
+        this.coursesDAO.SaveChanges();
+    }
+
+    public void ReloadTableData() {
+        this.coursesDAO.LoadDataTable();
     }
 
 
---
+++ 420DA3_A24_07453_Demo_Iterative/Presentation/Views/CoursView.cs
@@ -0,0 +1,33 @@
+﻿using _420DA3_Demo_Iterative.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _420DA3_Demo_Iterative.Presentation.Views;
+public partial class CoursView : Form {
+    private CoursService service;
+
+    public CoursView(CoursService service) {
+        this.service = service;
+        this.InitializeComponent();
+        this.dataGridView1.DataSource = this.service.GetCoursDataTable();
+    }
+
+    private void ButtonReload_Click(object sender, EventArgs e) {
+        this.service.ReloadTableData();
+    }
+
+    private void ButtonSaveChanges_Click(object sender, EventArgs e) {
+        this.service.SaveChanges();
+    }
+
+    private void BoutonQuitter_Click(object sender, EventArgs e) {
+        this.DialogResult = DialogResult.OK;
+    }
+}
